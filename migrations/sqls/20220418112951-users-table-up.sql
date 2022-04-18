cd02e1a98f4a53acf175260c9f5658d1656bede9
Fadi Beshay <fady.ayad.90@gmail.com>
2022-04-18 19:06:34 +0200

    chore: Update users table migrations

--- migrations/sqls/20220418112951-users-table-up.sql
+++ migrations/sqls/20220418112951-users-table-up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
     firstName VARCHAR(100) NOT NULL,
     lastName VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
