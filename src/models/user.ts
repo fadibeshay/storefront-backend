466da988849f9ff64026d7720a46b89f02f51db2
Fadi Beshay <fady.ayad.90@gmail.com>
2022-04-18 20:44:52 +0200

    feat: Add index and show methods to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,20 +3,52 @@ import Client from '../database';
 
 const { BCRYPT_PASSWORD, SALT_ROUNDS } = process.env;
 
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
 export interface User {
   id?: number;
   username: string;
   firstname: string;
   lastname: string;
+  role?: UserRole;
   password: string;
 }
 
 export class userStore {
+  async index(): Promise<User[]> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'SELECT * FROM users';
+      const result = await conn.query(sql);
+      conn.release();
+
+      return result.rows;
+    } catch (error) {
+      throw new Error(`Couldn't get the users. ${error}`);
+    }
+  }
+
+  async show(id: number): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql = 'SELECT * FROM users WHERE id=$1';
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      if (!result.rows[0]) throw new Error('The user id was not found.');
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Couldn't get user ${id}. ${error}`);
+    }
+  }
+
   async create(user: User): Promise<User> {
     try {
       const conn = await Client.connect();
       const sql =
-        'INSERT INTO users (username, firstname, lastname, password) VALUES ($1, $2, $3, $4) RETURNING *';
+        'INSERT INTO users (username, firstname, lastname, role, password) VALUES ($1, $2, $3, $4, $5) RETURNING *';
 
       const hash = await bcrypt.hash(
         user.password + BCRYPT_PASSWORD,
@@ -26,6 +58,7 @@ export class userStore {
         user.username,
         user.firstname,
         user.lastname,
+        user.role || UserRole.USER,
         hash,
       ]);
       conn.release();
